9b1d23b2ac61f7c46bb8f91238f3462f8249f39a
damko81 <damko81@gmail.com>
2024-06-26 09:00:14 +0200

    .

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Movie } from './movie';
+import { MovieService } from './movie.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent {
+export class MovieComponent implements OnInit {
+
+  public movies?: Movie[];
+
+  constructor(private movieService: MovieService){}
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
+  public getMovies(): void {
+    this.movieService.getMovies().subscribe(
+      (response: Movie[]) => {
+        this.movies = response;
+        console.log(this.movies);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
 
 }
--- src/app/movie/movie.service.ts
+++ src/app/movie/movie.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent} from '@angular/common/http';
+import { environment } from 'src/environments/environment.development';
+import { Movie } from './movie';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
+  
+  private apiServerUrl = environment.apiBaseUrl;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public getMovies(): Observable<Movie[]>{
+    return this.http.get<Movie[]>(`${this.apiServerUrl}/api/MovieAPI`);
+  }
+
 }
