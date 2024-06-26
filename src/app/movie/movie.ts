46f1fa81fe0788f3c12275e79ec7d3fef41d8d1b
damko81 <damko81@gmail.com>
2024-06-26 08:13:00 +0200

    .

---
+++ src/app/movie/movie.ts
@@ -0,0 +1,17 @@
+export interface Movie {
+    Id: number;
+    Disc: string;
+    Name: string;
+    NameFromDisc: string;
+    Genre: string;
+    Rating: string;
+    Description: string;
+    Stars: string;
+    Infobar: string;
+    Director: string;
+    Duration: string;
+    Storyline: string;
+    ReleaseDate: string;
+    Url: string;
+    ImageSrc: string;
+}
