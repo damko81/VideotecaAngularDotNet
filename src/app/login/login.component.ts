78cdc74577aad6b5dc3cc19caaa54ec0f071a5a9
damko81 <damko81@gmail.com>
2024-06-28 12:53:14 +0200

    .

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  public userName: string = "";
+  public password : string = "";
+  public errorMessage : string = 'Invalid Credentials';
+  public successMessage: string = "";
+  public invalidLogin: boolean = false;
+  public loginSuccess: boolean = false;
+  public authLoginSuccess: boolean = false;
+
+
+  constructor(
+              private http: HttpClient,
+              private router: Router,
+              private cookieService: CookieService
+             ) { }
+
+  ngOnInit(): void {
+    this.userName = this.cookieService.get("userName");
+  }
+
+  Login() {
+    
+  }
 
 }
